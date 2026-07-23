20b9f176377ebd86435e012b0ff033cb9a2b6139
pgpaisdev <pgpaisdev@gmail.com>
2026-07-23 20:17:52 +0100

    created basic movement strategies

---
+++ abilities/movement/basic_movement_strategy.gd
@@ -0,0 +1,7 @@
+class_name BasicMovementStrategy
+extends MovementStrategy
+
+@export var movement_direction: Vector2i
+
+func move(entity: Entity):
+    entity.move(movement_direction.x, movement_direction.y)
