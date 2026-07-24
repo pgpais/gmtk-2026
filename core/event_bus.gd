142b891f144a36e229f09a47228d5ac1dffc8654
pgpaisdev <pgpaisdev@gmail.com>
2026-07-24 15:45:32 +0100

    Created skeleton for enemies attacking frog_king

--- core/event_bus.gd
+++ core/event_bus.gd
@@ -4,3 +4,5 @@ signal ticker_new_tick(count: int)
 signal new_cycle
 signal entity_selected(entity: Entity)
 signal tile_selected(tile: Tile)
+
+signal enemy_attacked_frog_king(enemy: Enemy)
--- entities/enemies/enemy_data.gd
+++ entities/enemies/enemy_data.gd
@@ -1,6 +1,8 @@
 extends Resource
 class_name EnemyData
 
-@export var scene : PackedScene
-@export var move : int = 1
+@export var scene: PackedScene
+@export var move: int = 1
 #@export var move : MovementStrategy
+
+@export var frog_king_damage: int = 1
