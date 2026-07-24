13a7a5176adcfd4d1c26220b5627a9544844f8b6
pgpaisdev <pgpaisdev@gmail.com>
2026-07-24 12:28:47 +0100

    integrated movement strategy with AbilityRange. Added tiles in range function

--- abilities/movement/basic_movement_strategy.gd
+++ abilities/movement/basic_movement_strategy.gd
@@ -4,4 +4,8 @@ extends MovementStrategy
 @export var movement_direction: Vector2i
 
 func move(entity: Entity):
-	entity.move(movement_direction.x, movement_direction.y)
+	entity._move(movement_direction.x, movement_direction.y)
+
+func tiles_to_highlight(entity: Entity, grid_map: LayerGridMap) -> Array[Tile]:
+	var tiles: Array[Tile] = grid_map.get_tiles_in_ability_range(ability_range, entity.current_tile)
+	return tiles
