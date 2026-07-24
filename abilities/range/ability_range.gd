6fa47037b1303e206693afacf0a3ac82efe7893f
pgpaisdev <pgpaisdev@gmail.com>
2026-07-24 11:14:31 +0100

    created basic ability range data structure

---
+++ abilities/range/ability_range.gd
@@ -0,0 +1,20 @@
+class_name AbilityRange
+extends Resource
+
+@export var left_min_range: int
+@export var left_max_range: int
+@export var right_min_range: int
+@export var right_max_range: int
+@export var up_min_range: int
+@export var up_max_range: int
+@export var down_min_range: int
+@export var down_max_range: int
+
+@export var top_left_min_range: int
+@export var top_left_max_range: int
+@export var top_right_min_range: int
+@export var top_right_max_range: int
+@export var bottom_left_min_range: int
+@export var bottom_left_max_range: int
+@export var bottom_right_min_range: int
+@export var bottom_right_max_range: int
