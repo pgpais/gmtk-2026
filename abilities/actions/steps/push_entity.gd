a3968aea749b60f44b32c02910bdb0c7fc239c08
Daniel Reis <57777081+ds-reis@users.noreply.github.com>
2026-07-25 16:07:53 +0100

    Push Entity Action

---
+++ abilities/actions/steps/push_entity.gd
@@ -0,0 +1,10 @@
+class_name PushEntity
+extends Action
+
+func execute(action_handler: ActionHandler, entity: Entity, grid_map: LayerGridMap):
+	var tile: Tile = action_handler.get_parameter("selected_tile")
+	
+	var distance_to_tile: Vector2 = grid_map.get_tile_coordinates(tile) - grid_map.get_tile_coordinates(entity.current_tile)
+	var direction_to_entity = distance_to_tile.normalized()
+
+	tile.get_entity()._move(direction_to_entity.x, direction_to_entity.y)
