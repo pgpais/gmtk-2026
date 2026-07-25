e831d01a80cf9c14f8e5a43272d56eeb50aa6e75
AndreFPRodrigues <AndreFPRodrigues@users.noreply.github.com>
2026-07-25 19:28:25 +0100

    toggle change in shock commit

--- abilities/actions/steps/shock_action.gd
+++ abilities/actions/steps/shock_action.gd
@@ -1,7 +1,9 @@
 class_name ShockAction
 extends Action
 
-@export var toggle : bool = true
+#@export 
+var toggle : bool = false
 
 func execute(action_handler: ActionHandler, entity: Entity, grid_map: LayerGridMap):
+	toggle = !toggle
 	entity.shock(toggle)
