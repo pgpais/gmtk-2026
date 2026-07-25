cd75fff101d6bf8a48f530e230819e7a7d2b4881
David Gonçalves <davidslycooper@hotmail.com>
2026-07-25 21:42:00 +0100

    andré-2

--- abilities/actions/steps/spawn_entity.gd
+++ abilities/actions/steps/spawn_entity.gd
@@ -7,5 +7,6 @@ extends Action
 
 func execute(action_handler: ActionHandler, entity: Entity, grid_map: LayerGridMap):
 	var tile: Tile = action_handler.get_parameter(target_tile_parameter_name)
-
-	EventBus.request_enemy_spawn.emit(tile, enemy_data)
+	
+	if tile:
+		EventBus.request_enemy_spawn.emit(tile, enemy_data)
