8f0859f6a64a03a5ff935ce5a80536e0d28c90fb
pgpaisdev <pgpaisdev@gmail.com>
2026-07-25 20:51:39 +0100

    added parameter_name to shock_action

--- abilities/actions/steps/shock_action.gd
+++ abilities/actions/steps/shock_action.gd
@@ -1,7 +1,9 @@
 class_name ShockAction
 extends Action
 
+@export var parameter_name: String = "willShock"
 @export var toggle: bool = false
 
 func execute(action_handler: ActionHandler, entity: Entity, grid_map: LayerGridMap):
+	var toggle: bool = action_handler.get_parameter(parameter_name)
 	entity.shock(toggle)
