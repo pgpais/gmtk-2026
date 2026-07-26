d4efe167d309d8b815058f08d827efb86ed05585
pgpaisdev <pgpaisdev@gmail.com>
2026-07-26 16:22:33 +0100

    fixed null audioplayer

--- vfx/highlight/highlight.gd
+++ vfx/highlight/highlight.gd
@@ -12,5 +12,6 @@ func set_highlight(is_highlighted):
 		# custom_material.shader_parameter.aura_width = 0
 	else:
 		custom_material.set_shader_parameter("aura_width", 16)
-		audioPlayer.play()
+		if audioPlayer:
+			audioPlayer.play()
 		# custom_material.shader_parameter.aura_width = 4
