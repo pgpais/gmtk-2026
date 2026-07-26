d469b5a3d2c1f71294d2de164a90f3b53e8a1ec4
pgpaisdev <pgpaisdev@gmail.com>
2026-07-26 17:00:57 +0100

    inverted count. IT IS NOW A COUNTDOWN

--- ui/countdown/ticker_view.gd
+++ ui/countdown/ticker_view.gd
@@ -17,11 +17,11 @@ func _ready():
 
 func _on_new_tick(count):
 	var tween = create_tween()
-	
+	var inverted_count = ticker.max_count - count
 	# tween.tween_method(_change_label_scale, Vector2(1, 1), Vector2(2, 2), 0.2)
 	tween.tween_method(_change_label_color, Color(1, 1, 1, 1), highlight_color, 0.2)
 	tween.parallel().tween_callback(animator.play.bind("count"))
-	tween.tween_callback(Callable(func(text): label.text = str(text)).bind(count + 1))
+	tween.tween_callback(Callable(func(text): label.text = str(text)).bind(inverted_count))
 	tween.tween_method(_change_label_color, highlight_color, Color(1, 1, 1, 1), 0.2)
 
 func _change_label_scale(scale: Vector2):
